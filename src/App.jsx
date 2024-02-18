4d13e29065f052ecf07f976a66621584d940ad8f
misto <ottonellicopattibruno@gmail.com>
2024-02-18 13:56:59 -0300

    Handle creating and generating tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
-// import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  return <h1>To do list</h1>;
+  const [name, setName] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleNewTask = (event) => {
+    event.preventDefault();
+
+    const newTask = {
+      id: tasks.length + 1,
+      name: name,
+    };
+    setTasks(tasks.concat(newTask));
+    setName("");
+  };
+
+  return (
+    <div>
+      <h1>To do list</h1>
+      <form onSubmit={handleNewTask}>
+        <h2>Add task</h2>
+        <label>
+          Task name
+          <input value={name} onChange={handleNameChange} />
+        </label>
+      </form>
+      <h2>Tasks</h2>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
