2c7b7160f8edb20a968154b56785567ae91dfc19
misto <ottonellicopattibruno@gmail.com>
2024-02-18 19:54:24 -0300

    Render tasks by selected priority

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import Task from "./Task";
 
 const TaskList = ({ tasks, onDelete }) => {
+  const [priority, setPriority] = useState("");
+
+  const handlePriorityChange = (event) => {
+    setPriority(event.target.value);
+  };
+
+  const tasksToShow = tasks.filter((task) => task.priority.includes(priority));
   return (
-    <ul>
-      {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={() => onDelete(task.id)} />
-      ))}
-    </ul>
+    <div>
+      <label>
+        Show by priority
+        <select value={priority} onChange={handlePriorityChange}>
+          <option value="">All</option>
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option>
+        </select>
+      </label>
+      {tasksToShow.length > 0 ? (
+        <ul>
+          {tasksToShow.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              onDelete={() => onDelete(task.id)}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p>No tasks</p>
+      )}
+    </div>
   );
 };
 
